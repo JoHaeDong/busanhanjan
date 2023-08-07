3c03a4098334b815e6d7b6e1e7f4b69fcc2658fa
조해동 <johaedong@johaedong-ui-MacBookPro.local>
2023-08-07 10:13:01 +0900

    feat : subpage1

---
+++ js/alcohol.js
@@ -0,0 +1,13 @@
+function 클래스명 , 아이디명 + Handler(){
+  var 변수 = document.querySelector("클래스명 , 아이디명”);
+ }
+ 
+ 클래스명 , 아이디명 .addEventListener("click", function () {
+ 
+ });
+ 
+ function init (){
+ document.querySelector("클래스명 , 아이디명").onclick = 클래스명 , 아이디명 Handler;
+ }
+ 
+ window.onload = init;
