b5c0be4d3f477220253f7200ec1f882060c7f445
조해동 <johaedong@johaedong-ui-MacBookPro.local>
2023-08-07 11:30:08 +0900

    feat : subpage1

--- js/alcohol.js
+++ js/alcohol.js
@@ -1,13 +1,12 @@
-function 클래스명 , 아이디명 + Handler(){
-  var 변수 = document.querySelector("클래스명 , 아이디명”);
- }
- 
- 클래스명 , 아이디명 .addEventListener("click", function () {
- 
- });
- 
- function init (){
- document.querySelector("클래스명 , 아이디명").onclick = 클래스명 , 아이디명 Handler;
- }
- 
- window.onload = init;
+// '링크로 이동하기' 글자를 클릭했을 때 실행될 함수를 정의합니다.
+function home() {
+  // 이동할 링크의 URL을 지정합니다.
+  var home = '이동하고자 하는 링크의 경로 또는 URL';
+
+  // 현재 창을 이동할 링크의 URL로 변경합니다.
+  window.location.href = linkUrl;
+}
+
+// '링크로 이동하기' 글자를 클릭했을 때 moveToLink 함수가 실행되도록 합니다.
+var textElement = document.getElementById('home');
+textElement.addEventListener('click', home);
